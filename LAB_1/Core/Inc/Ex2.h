eab2050a71d5c220445a729f578cb51c02781a6c
Dang Phu <dangquocphupy26102004@gmail.com>
2024-09-10 23:37:05 +0700

    Init ex2 final

---
+++ LAB_1/Core/Inc/Ex2.h
@@ -0,0 +1,14 @@
+/*
+ * Ex2.h
+ *
+ *  Created on: Sep 10, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EX2_H_
+#define INC_EX2_H_
+#include "main.h"
+
+void EX2_RUN();
+
+#endif /* INC_EX2_H_ */
