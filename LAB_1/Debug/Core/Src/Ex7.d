719dd7fb86de6bb90fca6d96451ac26db47bed64
Dang Phu <dangquocphupy26102004@gmail.com>
2024-09-12 00:44:25 +0700

    Init ex7 final 0044 12/09/2024

--- LAB_1/Debug/Core/Src/Ex7.d
+++ LAB_1/Debug/Core/Src/Ex7.d
@@ -1,4 +1,4 @@
-Core/Src/Ex6.o: ../Core/Src/Ex6.c ../Core/Inc/Ex6.h ../Core/Inc/main.h \
+Core/Src/Ex7.o: ../Core/Src/Ex7.c ../Core/Inc/Ex7.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -21,7 +21,7 @@ Core/Src/Ex6.o: ../Core/Src/Ex6.c ../Core/Inc/Ex6.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
-../Core/Inc/Ex6.h:
+../Core/Inc/Ex7.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
